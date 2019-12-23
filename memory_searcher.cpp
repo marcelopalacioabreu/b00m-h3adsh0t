825b1b16b61536c8b1c62157559e6433e35aa952
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-12-23 16:24:42 -0500

    RPM WPM functions

--- memory_searcher.cpp
+++ memory_searcher.cpp
@@ -1,3 +1,6 @@
+DWORD pLocalPlayer = { 0x509B74 };
+DWORD playerObjectAddress = 0;
+
 // Base address of "client.dll" (int or DWORD)
 int ClientDLL = (int)GetModuleHandleA("client.dll");
 
@@ -6,4 +9,10 @@ EntityBase = *(int*)(ClientDLL + 0x9D3C6C);
 
 // Read the memory address of the EntityBase+0x23C4 to find the ID of the entity in the crosshair
 int in_cross = *(int*)(EntityBase + 0x23C4); 
+ 
+// ReadProcessMemory RPM Reads memory from a given process
+ReadProcessMemory(handleToGame, (LPCVOID)addressToRead, &variableToStoreReadInformation, sizeof(variableToStoreReadInformation), NULL);
+
+// WriteProcessMemory WPM writes memory to a given process
+WriteProcessMemory(handleToGame, (LPVOID)addressToWriteTo, &variableContainingValueToWrite, sizeof(variableContainingValueToWrite), NULL);
 
